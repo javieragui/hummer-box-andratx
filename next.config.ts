24496974a1b63b614a5c24f2c0aaa606b74c6785
Javier Aguilar <javieraguilararribas@outlook.com>
2025-02-13 14:34:22 +0100

    [Update] Cambiada la configuracion del next.config para las rutas

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,11 @@
 import type { NextConfig } from 'next';
 
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = "hummer-box-andratx"; // Nombre exacto de tu repositorio en GitHub
-
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'export',
   images: {
     unoptimized: true, // Evita problemas con imágenes en modo estático
-  },
-  basePath: isProd ? `/${repoName}` : '', // Se usa SOLO en producción
-  assetPrefix: isProd ? `/${repoName}/` : '',
+  }  
 };
 
 export default nextConfig;
